795c47732c11299161ebc1d2fec410b345f66c87
Shikhsaid <ramazanov189@gmail.com>
2025-03-23 15:49:16 +0300

    Sekond

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//TIP To
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +11,6 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
     }
 }
